7d0841d7d1e1118ad8d69ba1eb28890182e84c8a
gbui <gbui@users.noreply.github.com>
2018-08-25 15:36:56 -0700

    Fix mixin bootstrap exception and add CurseGradle

--- src/main/java/yalter/mousetweaks/rift/MouseTweaksRiftCore.java
+++ src/main/java/yalter/mousetweaks/rift/MouseTweaksRiftCore.java
@@ -1,11 +1,13 @@
 package yalter.mousetweaks.rift;
 
 import org.dimdev.riftloader.listener.InitializationListener;
+import org.spongepowered.asm.launch.MixinBootstrap;
 import org.spongepowered.asm.mixin.Mixins;
 
 public class MouseTweaksRiftCore implements InitializationListener {
     @Override
     public void onInitialization() {
+        MixinBootstrap.init();
         Mixins.addConfiguration("mixins.mousetweaks.json");
     }
 }
